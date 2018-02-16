4428afa0af0633a0285f1372a9244711f9ff9016
Sol Boucher <sboucher@cmu.edu>
2018-02-16 01:11:52 -0500

    src/stdlib.rs: Eliminate the infinite recursion during the initialization of dynamic allocation interposition
    Note, however, that this approach isn't thread safe because the symbols are stored locally to each
    thread, and we instruct dlsym() to use a static libc buffer as a working space during symbol lookup.

--- src/stdlib.rs
+++ src/stdlib.rs
@@ -24,25 +24,76 @@ thread_local! {
 
 #[no_mangle]
 pub unsafe extern "C" fn malloc(size: size_t) -> *mut c_void {
-	FUNS.with(|funs| funs.malloc)(size)
+	shallow_call(|| FUNS.with(|funs| funs.malloc)(size))
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn calloc(nobj: size_t, size: size_t) -> *mut c_void {
-	FUNS.with(|funs| funs.calloc)(nobj, size)
+	shallow_call(|| FUNS.with(|funs| funs.calloc)(nobj, size))
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn realloc(addr: *mut c_void, size: size_t) -> *mut c_void {
-	FUNS.with(|funs| funs.realloc)(addr, size)
+	shallow_call(|| FUNS.with(|funs| funs.realloc)(addr, size))
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn posix_memalign(addr: *mut *mut c_void, align: size_t, size: size_t) -> c_int {
-	FUNS.with(|funs| funs.posix_memalign)(addr, align, size)
+	shallow_call(|| FUNS.with(|funs| funs.posix_memalign)(addr, align, size))
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn free(addr: *mut c_void) {
-	FUNS.with(|funs| funs.free)(addr);
+	shallow_call(|| FUNS.with(|funs| funs.free)(addr));
+}
+
+/// "Thunk," guarding against mutual recursion.
+///
+/// We have a bootstrapping crisis: `dlsym()` allocates memory during the initialization of `FUNS`!
+/// Clearly we can't permit the nested call because it, too, would find `FUNS` uninitialized and
+/// incite infinite recursion.  Fortunately, glibc's `dlsym()` implementation gracefully falls back
+/// to using a static buffer if its allocation request fails (see `dlerror.c:_dlerror_run()`),
+/// behavior that we can exploit by returning `NULL` upon detecting (potentially) mutual recursion.
+/// The only sticky situation to handle is threads that simultaneously attempt to allocate memory
+/// *for the first time*.
+fn shallow_call<T: Optional, F: Fn() -> T>(thunk: F) -> T {
+	use std::cell::Cell;
+
+	thread_local! {
+		static RECURSING: Cell<bool> = Cell::new(false);
+	}
+
+	if RECURSING.with(|recursing| recursing.replace(true)) {
+		T::none()
+	} else {
+		let res = thunk();
+		RECURSING.with(|recursing| recursing.set(false));
+		res
+	}
+}
+
+trait Optional {
+	fn none() -> Self;
+}
+
+impl Optional for () {
+	fn none() -> Self {
+		()
+	}
+}
+
+impl Optional for i32 {
+	fn none() -> Self {
+		use libc::ENOMEM;
+
+		ENOMEM
+	}
+}
+
+impl<T> Optional for *mut T {
+	fn none() -> Self {
+		use std::ptr::null_mut;
+
+		null_mut()
+	}
 }
