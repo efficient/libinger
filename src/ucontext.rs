a318a0dac6b4525ca37bf9298d11d5ce3cc93a3c
Sol Boucher <sboucher@cmu.edu>
2018-03-01 01:35:33 -0500

    src/ucontext.rs: Remove cpycontext(), since we're using std::mem::swap() instead

--- src/ucontext.rs
+++ src/ucontext.rs
@@ -7,14 +7,6 @@ unsafe impl Zeroable for ucontext_t {}
 
 pub const REG_CSGSFS: usize = 18;
 
-pub fn cpycontext(dest: &mut ucontext_t, src: &ucontext_t) {
-	dest.uc_stack = src.uc_stack;
-
-	let segs = dest.uc_mcontext.gregs[REG_CSGSFS];
-	dest.uc_mcontext = src.uc_mcontext;
-	dest.uc_mcontext.gregs[REG_CSGSFS] = segs;
-}
-
 pub fn getcontext(context: &mut ucontext_t) -> Result<()> {
 	use libc::getcontext;
 
