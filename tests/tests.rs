33274b56cc863eaaa0a89d473d4cec3cadbc0862
Sol Boucher <sboucher@cmu.edu>
2018-08-09 02:04:42 -0400

    tests/tests.rs: Add a makecontext() test

--- tests/tests.rs
+++ tests/tests.rs
@@ -2,18 +2,21 @@
 
 extern crate ucontext;
 
+use ucontext::getcontext;
+use ucontext::makecontext;
+use ucontext::setcontext;
+
 fn main() {
 	getcontext_donothing();
 	getcontext_setcontext();
 	getcontext_succeedatnothing();
 	//getcontext_nested();
+	makecontext_setcontext();
 }
 
 #[cfg_attr(test, should_panic(expected = "done"))]
 #[cfg_attr(test, test)]
 fn getcontext_donothing() {
-	use ucontext::getcontext;
-
 	let mut reached = false;
 	getcontext(|_| reached = true, || unreachable!()).unwrap();
 	assert!(reached);
@@ -23,9 +26,6 @@ fn getcontext_donothing() {
 #[cfg_attr(test, should_panic(expected = "done"))]
 #[cfg_attr(test, test)]
 fn getcontext_setcontext() {
-	use ucontext::getcontext;
-	use ucontext::setcontext;
-
 	let mut reached = false;
 	getcontext(
 		|context| {
@@ -41,9 +41,6 @@ fn getcontext_setcontext() {
 #[cfg_attr(test, should_panic(expected = "done"))]
 #[cfg_attr(test, test)]
 fn getcontext_succeedatnothing() {
-	use ucontext::getcontext;
-	use ucontext::setcontext;
-
 	let invalid = getcontext(|context| context, || unreachable!()).unwrap();
 	assert!(setcontext(invalid).is_none());
 	panic!("done");
@@ -54,8 +51,6 @@ fn getcontext_succeedatnothing() {
 #[cfg_attr(test, test)]
 fn getcontext_nested() {
 	use std::cell::Cell;
-	use ucontext::getcontext;
-	use ucontext::setcontext;
 
 	let mut reached = true;
 	let context = Cell::new(None);
@@ -73,6 +68,35 @@ fn getcontext_nested() {
 			unreachable!();
 		},
 	).unwrap();
+	assert!(reached);
+	panic!("done");
+}
+
+#[cfg_attr(test, should_panic(expected = "done"))]
+#[cfg_attr(test, test)]
+fn makecontext_setcontext() {
+	use std::cell::Cell;
+	use ucontext::MINSIGSTKSZ;
+
+	thread_local! {
+		static REACHED: Cell<bool> = Cell::new(false);
+	}
+
+	extern "C" fn call() {
+		REACHED.with(|reached| reached.set(true));
+	}
+
+	let mut reached = false;
+	getcontext(
+		|mut successor| {
+			let mut stack = [0u8; MINSIGSTKSZ];
+			let predecessor = makecontext(call, &mut stack, Some(&mut successor)).unwrap();
+			setcontext(predecessor);
+			unreachable!();
+		},
+		|| reached = true,
+	).unwrap();
+	assert!(REACHED.with(|reached| reached.get()));
 	assert!(reached);
 	panic!("done");
 }
