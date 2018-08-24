3044771732c41c6d6796a86b27a5a900af62cb8a
Sol Boucher <sboucher@cmu.edu>
2018-08-24 04:40:34 -0400

    src/ucontext.rs: Add tests for ucontext_t move and swap invariants
    This structure contains a self-referential pointer, which we must fix after taking any action (even
    an implicit one) that might cause it to be incorrect.  Such a correction must be made by the time we
    attempt to call the ucontext_t.

--- src/ucontext.rs
+++ src/ucontext.rs
@@ -64,3 +64,58 @@ impl<S: DerefMut<Target = [u8]>> Context<S> {
 		unimplemented!();
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use libc::ucontext_t;
+	use super::getcontext;
+	use ucontext::HandlerContext;
+
+	#[test]
+	fn context_moveinvariant() {
+		use invar::MoveInvariant;
+
+		let mut context = getcontext(|context| context, || unreachable!()).unwrap();
+		let mut context = context.context.borrow_mut();
+		assert!(! uc_inbounds(context.uc_mcontext.fpregs as _, &*context));
+		context.after_move();
+		assert!(uc_inbounds(context.uc_mcontext.fpregs as _, &*context));
+	}
+
+	#[test]
+	fn context_swapinvariant() {
+		use invar::MoveInvariant;
+		use std::mem::transmute;
+
+		let mut first = getcontext(|context| context, || unreachable!()).unwrap();
+		let second = getcontext(|context| context, || unreachable!()).unwrap();
+		let mut second = HandlerContext (second.context.into_inner());
+		{
+			let mut first = first.context.borrow_mut();
+			let HandlerContext (second) = &mut second;
+			assert!(! uc_inbounds(first.uc_mcontext.fpregs as _, &*first));
+			assert!(! uc_inbounds(second.uc_mcontext.fpregs as _, second));
+
+			first.after_move();
+			second.after_move();
+			first.uc_link = first.uc_mcontext.fpregs as _;
+			second.uc_link = second.uc_mcontext.fpregs as _;
+			assert!(uc_inbounds(first.uc_link, &*first));
+			assert!(uc_inbounds(second.uc_link, second));
+		}
+		first.swap(&mut second);
+
+		let first = first.context.borrow();
+		let HandlerContext (second) = &mut second;
+		assert!(uc_inbounds(first.uc_mcontext.fpregs as _, &*first));
+		assert!(uc_inbounds(second.uc_mcontext.fpregs as _, second));
+		assert!(uc_inbounds(first.uc_link, second));
+		assert!(uc_inbounds(second.uc_link, &*first));
+	}
+
+	fn uc_inbounds(within: *const ucontext_t, context: *const ucontext_t) -> bool {
+		within > context && within < unsafe {
+			context.add(1)
+		}
+	}
+}
