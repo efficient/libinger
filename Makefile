8bde61f75e95940748acc6d8bd24797520b5e31f
Sol Boucher <sboucher@cmu.edu>
2018-11-08 07:18:33 -0500

    Add Rust component on top of the C portions of the library
    For now this consists only of wrappers atop the public C interfaces.

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 BINDGEN := bindgen
+RUSTC := rustc
 
 override BINDFLAGS := --default-enum-style rust $(BINDFLAGS)
 override CFLAGS := -std=c11 -O2 -Wall -Wextra -Wpedantic $(CFLAGS)
+override RUSTFLAGS := -O $(RUSTFLAGS)
+
+libgotcha.rlib: private RUSTFLAGS += -L.
+libgotcha.rlib: private LDLIBS += -lmirror_object
+libgotcha.rlib: libmirror_object.a mirror.rs
 
 libmirror_object.a: error.o mirror_object_containing.o
 
@@ -24,5 +30,11 @@ clean:
 lib%.a: %.o
 	$(AR) rs $@ $^
 
+lib%.rlib: %.rs
+	$(RUSTC) -Clink-args="$(LDFLAGS)" $(RUSTFLAGS) --crate-type rlib $< $(LDLIBS)
+
 lib%.so: %.o
 	$(CC) $(LDFLAGS) -shared -o $@ $^ $(LDLIBS)
+
+lib%.so: %.rs
+	$(RUSTC) -Clink-args="$(LDFLAGS)" $(RUSTFLAGS) --crate-type dylib -Cprefer-dynamic $< $(LDLIBS)
---
+++ gotcha.rs
@@ -0,0 +1,94 @@
+mod mirror;
+
+pub use mirror::error;
+#[doc(hidden)]
+pub use mirror::link_map;
+use std::ffi::CStr;
+use std::ffi::CString;
+use std::fmt::Debug;
+use std::fmt::Error as FmtError;
+use std::fmt::Formatter;
+use std::os::raw::c_char;
+
+pub struct Error {
+	pub error: error,
+	message: &'static CStr,
+	explanation: Option<CString>,
+}
+
+#[derive(Clone, Copy)]
+pub struct ObjectFile (*const link_map);
+
+pub unsafe fn mirror_object_containing<T>(function: &T) -> Result<(), Error> {
+	use mirror::mirror_object;
+	use std::mem::transmute;
+
+	let mirror_object: unsafe extern "C" fn(_, _) -> _ = mirror_object;
+	let mirror_object: extern "C" fn(_, _) -> _ = transmute(mirror_object);
+	test_object_containing(mirror_object, function)
+}
+
+#[doc(hidden)]
+pub fn test_object_containing<T>(
+	plugin: extern "C" fn(*const link_map, *const c_char) -> error,
+	function: &T
+) -> Result<(), Error> {
+	use mirror::test_object_containing;
+
+	Error::from(unsafe {
+		test_object_containing(Some(plugin), function as *const _ as _)
+	})
+}
+
+pub unsafe fn mirror_object(object: ObjectFile, path: Option<&CStr>) -> Result<(), Error> {
+	use mirror::mirror_object;
+	use std::ptr::null;
+
+	let ObjectFile (object) = object;
+	Error::from(mirror_object(object, path.map(|path| path.as_ptr()).unwrap_or(null())))
+}
+
+impl Error {
+	fn from(error: error) -> Result<(), Error> {
+		use mirror::error_explanation;
+		use mirror::error_message;
+
+		if error == error::SUCCESS {
+			Ok(())
+		} else {
+			let message = unsafe {
+				error_message(error)
+			};
+			let explanation = unsafe {
+				error_explanation(error)
+			};
+			debug_assert!(! message.is_null());
+
+			let message = unsafe {
+				CStr::from_ptr(message)
+			};
+			let explanation = if explanation.is_null() {
+				None
+			} else {
+				Some(unsafe {
+					CStr::from_ptr(explanation)
+				}.to_owned())
+			};
+			Err(Self {
+				error,
+				message,
+				explanation,
+			})
+		}
+	}
+}
+
+impl Debug for Error {
+	fn fmt(&self, form: &mut Formatter) -> Result<(), FmtError> {
+		if let Some(explanation) = self.explanation.as_ref() {
+			write!(form, "{:?}: {:?}", self.message, explanation)
+		} else {
+			write!(form, "{:?}", self.message)
+		}
+	}
+}
