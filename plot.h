2c16a7d8c1304f0966f527537a75f82aa1d01534
Sol Boucher <sboucher@cmu.edu>
2019-01-15 10:34:05 -0500

    Restructure plot structure for easier access to its goot and simpler byte-granularity pointer arithmetic

--- plot.h
+++ plot.h
@@ -8,8 +8,17 @@
 #include <stddef.h>
 #include <stdint.h>
 
-extern struct goot *const plot_template[];
+struct handle;
+
+struct plot {
+	struct goot *goot;
+	const uint8_t code[];
+};
+
+extern const struct plot plot_template;
 extern const size_t plot_size;
+
+// Relative to code member.
 extern const size_t plot_entries_offset;
 extern const size_t plot_entry_size;
 #endif
