c0773ab18d49be4cfdfbd5ce888ed3e0511d3f89
Sol Boucher <sboucher@cmu.edu>
2019-04-26 18:32:29 -0400

    gotcha.mk: Fix horrible static library double-inclusion bug
    Due to an accidental use of $^ instead of $<, our static library was appearing twice in the
    dependency list of client libraries: once normally, and once with --whole-archive.  Only the latter
    had any effect unless the client library referenced any of our symbols, in which case it pulled in
    duplicates of a subset of our definitions.  This resulted in linker errors that, if manually
    overridden,
    could lead to a client library with multiple copies of our constructor.

--- gotcha.mk
+++ gotcha.mk
@@ -7,4 +7,4 @@ ELFUTILS_PATH := /usr/lib/x86_64-linux-gnu/elfutils
 endif
 
 lib%.so: %.o $(LIBGOTCHA_PATH)/libgotcha.a
-	$(CC) $(LDFLAGS) -shared -zdefs -zinitfirst -znodelete -znoexecstack -ztext -static-libgcc -L$(ELFUTILS_PATH) -Wl,-R$(ELFUTILS_PATH) -o $@ $^ $(LDLIBS) -Wl,--whole-archive $(LIBGOTCHA_PATH)/libgotcha.a -Wl,--no-whole-archive -l$(LIBSTDRUST_SONAME) -lasm -lc -ldl -lebl_x86_64 -lpthread
+	$(CC) $(LDFLAGS) -shared -zdefs -zinitfirst -znodelete -znoexecstack -ztext -static-libgcc -L$(ELFUTILS_PATH) -Wl,-R$(ELFUTILS_PATH) -o $@ $< $(LDLIBS) -Wl,--whole-archive $(LIBGOTCHA_PATH)/libgotcha.a -Wl,--no-whole-archive -l$(LIBSTDRUST_SONAME) -lasm -lc -ldl -lebl_x86_64 -lpthread
