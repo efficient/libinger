6988fd290ed9f27d551e566cd456cfc31637b741
Sol Boucher <sboucher@cmu.edu>
2019-04-26 02:51:12 -0400

    gotcha.mk: Tweak link configuration so client libraries' dynamic sections are more similar to our own
    This marks the stack as nonexecutable, removes the dependency on libgcc_s, and reorders libc and
    ld-linux to be earlier in the dependency list (and notably before libpthread).  The only remaining
    difference is our own explicit dependence on libunwind and declaration of a soname.

--- gotcha.mk
+++ gotcha.mk
@@ -7,4 +7,4 @@ ELFUTILS_PATH := /usr/lib/x86_64-linux-gnu/elfutils
 endif
 
 lib%.so: %.o $(LIBGOTCHA_PATH)/libgotcha.a
-	$(CC) $(LDFLAGS) -shared -zdefs -zinitfirst -znodelete -ztext -L$(ELFUTILS_PATH) -Wl,-R$(ELFUTILS_PATH) -o $@ $^ $(LDLIBS) -Wl,--whole-archive $(LIBGOTCHA_PATH)/libgotcha.a -Wl,--no-whole-archive -lasm -ldl -lebl_x86_64 -lpthread -l$(LIBSTDRUST_SONAME)
+	$(CC) $(LDFLAGS) -shared -zdefs -zinitfirst -znodelete -znoexecstack -ztext -static-libgcc -L$(ELFUTILS_PATH) -Wl,-R$(ELFUTILS_PATH) -o $@ $^ $(LDLIBS) -Wl,--whole-archive $(LIBGOTCHA_PATH)/libgotcha.a -Wl,--no-whole-archive -l$(LIBSTDRUST_SONAME) -lasm -lc -ldl -lebl_x86_64 -lpthread
