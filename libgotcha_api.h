015189525758f81f7b78d1b50478d519059fde4d
Sol Boucher <sboucher@cmu.edu>
2019-04-29 12:21:07 -0400

    Document "public" headers

--- libgotcha_api.h
+++ libgotcha_api.h
@@ -1,3 +1,22 @@
+// "Public" interface to the libgotcha GOT intercept library.
+//
+// Despite the marketing, this API isn't really for general consumption.  The library becomes active
+// automatically and intercepts all subsequent inter--object file activity as soon as it is loaded
+// into the process image.  This can happen either by virtue of some other object file in the
+// application's dependency graph depending on libgotcha, or due to the inclusion of libgotcha or
+// a dependent in LD_PRELOAD).  Statically linking the main binary against libgotcha is unsupported,
+// as is loading it at runtime via the loader's dlopen() interface.
+//
+// Just having libgotcha active isn't actually good for anything unless you like needless overhead.
+// That's where you come in: special *client libraries* can use this API to configure libgotcha for
+// their own use case.  Such libraries (up to a limit of one per process image) may choose to
+// *statically* link against libgotcha.  This confers two main advantages for this client: it
+// becomes a singleton whose calls and accesses are always performed in the shared namespace, and
+// any function definitions automatically shadow all other instances in the process image (but from
+// an internal perspective, are shadowed by the next definition).  Note that a statically-linked
+// client library is not subject to the interpositions imposed by libgotcha on the rest of the
+// application; if this is not desired, see the supplementary libgotcha_repl.h header.
+
 #ifndef LIBGOTCHA_H_
 #define LIBGOTCHA_H_
 
--- libgotcha_repl.h
+++ libgotcha_repl.h
@@ -1,3 +1,24 @@
+// Function replacement interface for the libgotcha GOT intercept library.
+//
+// You don't need this unless you're writing a statically-linked client library that needs to call
+// one of libgotcha's library function wrappers.  By default, such library's calls are retargeted at
+// the interposed external definition, but this behavior can be overridden by making your calls with
+// the special names declared herein.  Note that you will first need to expose the symbols, e.g. by
+// invoking objcopy --globalize-symbol on the libgotcha static library.
+//
+// This header may be especially useful to client libraries that need to interpose on the same
+// symbols as libgotcha, as it allows them to defer to the libgotcha implementations when
+// appropriate.  In order to override the symbols, it is first necessary to weaken the corresponding
+// library symbols, e.g. by invoking objcopy -W on the libgotcha static library.
+//
+// If a client library wants to interpose a symbol not already wrapped by libgotcha, or wants to
+// interpose one but not provide access to libgotcha's own implementation---danger, W.R.!---it
+// doesn't need this header at all: instead, it should just make what appears to be a recursive
+// call.  To prevent the C compiler from optimizing away such a call, the containing module should
+// be compiled with the -fno-optimize-sibling-calls switch and *without* the -fpic or -fPIC one.  If
+// the latter is not possible, it may be necessary to create an alias of the wrapping definition,
+// e.g. using an attribute or pragma.
+
 #ifndef LIBGOTCHA_REPL_H_
 #define LIBGOTCHA_REPL_H_
 
