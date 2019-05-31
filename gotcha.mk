3c42c026ddd0288a5eb56035c6e2c8357450124c
Sol Boucher <sboucher@cmu.edu>
2019-05-31 03:30:28 -0400

    gotcha.mk: Fix a bug where Rust client libraries linked against libgotcha.so if it was present
    instead of always linking against libgotcha.rlib.  The problem was caused by rustc's
    -Cprefer-dynamic switch, which is required to link against the whole libgotcha.rlib archive.
    
    Rather than searching the entire libgotcha directory, this patch copies the appropriate static
    library into the client source tree before building each client library.

--- gotcha.mk
+++ gotcha.mk
@@ -14,8 +14,12 @@ ifndef RUSTFLAGS
 RUSTFLAGS := -O
 endif
 
-lib%.so: %.o $(LIBGOTCHA_PATH)/libgotcha.a
-	$(CC) $(LDFLAGS) -shared -zdefs -zinitfirst -znodelete -znoexecstack -ztext -static-libgcc -L$(ELFUTILS_PATH) -Wl,-R$(ELFUTILS_PATH) -o $@ $< $(LDLIBS) -Wl,--whole-archive $(LIBGOTCHA_PATH)/libgotcha.a -Wl,--no-whole-archive -l$(LIBSTDRUST_SONAME) -lasm -lc -ldl -lebl_x86_64 -lpthread
+lib%.so: %.o libgotcha.a
+	$(CC) $(LDFLAGS) -shared -zdefs -zinitfirst -znodelete -znoexecstack -ztext -static-libgcc -L. -L$(ELFUTILS_PATH) -Wl,-R$(ELFUTILS_PATH) -o $@ $< $(LDLIBS) -Wl,--whole-archive -lgotcha -Wl,--no-whole-archive -l$(LIBSTDRUST_SONAME) -lasm -lc -ldl -lebl_x86_64 -lpthread
 
-lib%.so: %.rs $(LIBGOTCHA_PATH)/libgotcha.rlib
-	$(RUSTC) $(RUSTFLAGS) --crate-type dylib -Clink-args="$(LDFLAGS) -zdefs -zinitfirst -znodelete -ztext -Wl,-zlazy -L$(ELFUTILS_PATH) -Wl,-R$(ELFUTILS_PATH) -lasm -lebl_x86_64" -Cprefer-dynamic -L$(LIBGOTCHA_PATH) $<
+lib%.so: %.rs libgotcha.rlib
+	$(RM) libgotcha.so
+	$(RUSTC) $(RUSTFLAGS) --crate-type dylib -Clink-args="$(LDFLAGS) -zdefs -zinitfirst -znodelete -ztext -Wl,-zlazy -L$(ELFUTILS_PATH) -Wl,-R$(ELFUTILS_PATH) -lasm -lebl_x86_64" -Cprefer-dynamic -L. $<
+
+libgotcha.%: $(LIBGOTCHA_PATH)/libgotcha.%
+	cp $< $@
