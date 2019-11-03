71edbdda2115ef96ea258ab1650d08aad2cac64b
Sol Boucher <sboucher@cmu.edu>
2019-11-03 17:36:03 -0500

    benches/inger.rs: Actually write resume() benchmark
    It just gives the total time taken by each call.

--- benches/inger.rs
+++ benches/inger.rs
@@ -7,13 +7,23 @@ use bencher::Bencher;
 benchmark_group![bench, resume];
 
 fn resume(lo: &mut Bencher) {
-	use inger::Linger;
+	use inger::launch;
+	use inger::pause;
 	use inger::resume;
+	use std::sync::atomic::AtomicBool;
+	use std::sync::atomic::Ordering;
 
-	let mut linger: Linger<_, fn(_)> = Linger::Completion(());
+	let run = AtomicBool::from(true);
+	let mut linger = launch(|| while run.load(Ordering::Relaxed) {
+		pause();
+	}, u64::max_value()).unwrap();
+
 	lo.iter(||
 		drop(resume(&mut linger, u64::max_value()))
 	);
+
+	run.store(false, Ordering::Relaxed);
+	resume(&mut linger, u64::max_value()).unwrap();
 }
 
 benchmark_main! {
