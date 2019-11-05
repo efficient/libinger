df271d22e84c0ad76822c46cf796c9c49601cd8f
Sol Boucher <sboucher@cmu.edu>
2019-11-04 22:29:03 -0500

    benchmark.c: Break out unmemoize into multiple helpers in preparation for fixing benchmarks
    At some point, there was a bug in benchmarks that "fixed" its behavior, but that has since been
    fixed.  As such, libbenchmark.so needs to save a copy of both the GOT and SGOT, which means it needs
    to look up the latter before it calls the libgotcha constructor.

--- benchmark.c
+++ benchmark.c
@@ -1,18 +1,14 @@
 #include <sys/mman.h>
 #include <assert.h>
 #include <link.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-static void (**got)(void);
-static void (*nope)(void);
-
-static void (**unmemoize(void (**mem)(void), const char *sym))(void) {
-	const struct link_map *l = dlopen(NULL, RTLD_LAZY);
-	if(!l)
-		return NULL;
+static inline const ElfW(Rela) *jmprel(const struct link_map *l, const char *s) {
 	const ElfW(Dyn) *d;
 
 	for(d = l->l_ld; d->d_tag != DT_STRTAB; ++d)
@@ -36,35 +32,53 @@ static void (**unmemoize(void (**mem)(void), const char *sym))(void) {
 	const ElfW(Rela) *end = (ElfW(Rela) *) ((uintptr_t) rel + d->d_un.d_val);
 
 	const ElfW(Rela) *r;
-	for(r = rel; strcmp(sym, str + st[ELF64_R_SYM(r->r_info)].st_name); ++r)
+	for(r = rel; strcmp(s, str + st[ELF64_R_SYM(r->r_info)].st_name); ++r)
 		if(r + 1 == end)
 			return NULL;
+	return r;
+}
 
-	// Return the address of the GOT entry containing the address of the PLT trampoline.
-	void (**res)(void) = (void (**)(void)) (l->l_addr + r->r_offset);
-
+static bool unmemoize(void (**shadow)(void), const struct link_map *l, const ElfW(Rela) *r) {
 	size_t pgsz = sysconf(_SC_PAGESIZE);
 	void *page = (void *) ((uintptr_t) &r->r_offset & ~(pgsz - 1));
 	if(mprotect(page, pgsz, PROT_READ | PROT_WRITE))
-		return NULL;
+		return false;
 
-	// Trick future calls to the PLT trampoline into updating nope in lieu of the GOT entry.
-	((ElfW(Rela) *) r)->r_offset = (uintptr_t) mem - l->l_addr;
+	// Trick future calls to the PLT trampoline into updating mem in lieu of the GOT entry.
+	((ElfW(Rela) *) r)->r_offset = (uintptr_t) shadow - l->l_addr;
 
 	if(mprotect(page, pgsz, PROT_READ))
-		return NULL;
+		return false;
 
-	return res;
+	return true;
 }
 
+static inline void (**addr(const struct link_map *l, const ElfW(Rela) *r))(void) {
+	return (void (**)(void)) (l->l_addr + r->r_offset);
+}
+
+static void (**got)(void);
+static void (*nope)(void);
+
 static void __attribute__((constructor)) ctor(void) {
+	const struct link_map *l = dlopen(NULL, RTLD_LAZY);
+	assert(l);
+
+	const ElfW(Rela) *r = jmprel(l, "nop");
+	assert(r);
+
 	// The loader initializes dependencies before LD_PRELOADs... unless the latter have
-	// INITFIRST set... unless *any* of the former has INITFIRST set.  Unfortunately, we're in
-	// the latter case thanks to libpthread.  However, dlopen()'ing libgotcha here appears to
-	// call its constructor immediately, even though it was already open.
+	// INITFIRST set... unless *any* of the former has INITFIRST set.  Fortunately, we're in the
+	// latter case thanks to libpthread.  However, dlopen()'ing libgotcha here appears to
+	// call its constructor immediately, even though it was already open.  We've already saved
+	// the address of the (real) GOT entry, so let's go ahead and do that now!
 	dlopen("libgotcha.so", RTLD_LAZY | RTLD_NOLOAD);
 
-	assert(got = unmemoize(&nope, "nop"));
+	if(!unmemoize(&nope, l, r))
+		abort();
+
+	// Save the address of the GOT entry containing the address of the PL(O)T trampoline.
+	got = addr(l, r);
 }
 
 void nop(void) {}
