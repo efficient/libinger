bef5fd31050e4b56e755d69c7c9e21e6db51304a
Sol Boucher <sboucher@cmu.edu>
2019-12-05 03:35:23 -0500

    benches/baseline.rs: Add pthread_join() overhead benchmark

--- benches/baseline.rs
+++ benches/baseline.rs
@@ -5,9 +5,11 @@ extern crate libc;
 use bencher::Bencher;
 use libc::exit;
 use libc::waitpid;
+use std::ffi::c_void;
+use std::ptr::null;
 use std::ptr::null_mut;
 
-benchmark_group![bench, fork, vfork, pthread_create];
+benchmark_group![bench, fork, vfork, pthread_create, pthread_join];
 
 fn fork(lo: &mut Bencher) {
 	use libc::fork;
@@ -48,10 +50,7 @@ fn vfork(lo: &mut Bencher) {
 fn pthread_create(lo: &mut Bencher) {
 	use libc::pthread_create;
 	use libc::pthread_join;
-	use std::ffi::c_void;
-	use std::ptr::null;
 
-	extern fn identity(val: *mut c_void) -> *mut c_void { val }
 	lo.iter(|| {
 		let mut tid = 0;
 		unsafe {
@@ -60,6 +59,28 @@ fn pthread_create(lo: &mut Bencher) {
 		}
 	})
 }
+
+fn pthread_join(lo: &mut Bencher) {
+	use libc::pthread_create;
+	use libc::pthread_join;
+	use std::collections::VecDeque;
+
+	let mut tids: VecDeque<_> = (0..100_000).map(|_| {
+		let mut tid = 0;
+		unsafe {
+			pthread_create(&mut tid, null(), identity, null_mut())
+		};
+		tid
+	}).collect();
+	lo.iter(|| {
+		let tid = tids.pop_front().unwrap();
+		unsafe {
+			pthread_join(tid, null_mut());
+		}
+	});
+}
+
+extern fn identity(val: *mut c_void) -> *mut c_void { val }
 
 benchmark_main! {
 	bench
