211d5aedba23ff437ebaf5220b66d16fbb44d969
Sol Boucher <sboucher@cmu.edu>
2019-12-05 03:02:00 -0500

    benches/baseline.rs: Add vfork() benchmark to gauge the overhead of waitpid()

--- benches/baseline.rs
+++ benches/baseline.rs
@@ -2,20 +2,43 @@
 extern crate bencher;
 
 use bencher::Bencher;
+use std::os::raw::c_int;
 
-benchmark_group![bench, fork];
+extern {
+		fn exit(_: c_int) -> !;
+		fn waitpid(_: c_int, _: usize, _: c_int);
+}
+
+benchmark_group![bench, fork, vfork];
 
 fn fork(lo: &mut Bencher) {
-	use std::os::raw::c_int;
 	extern {
-		fn exit(_: c_int) -> !;
 		fn fork() -> c_int;
-		fn waitpid(_: c_int, _: usize, _: c_int);
 	}
 
 	lo.iter(|| {
 		let pid = unsafe {
 			fork()
+		};
+		if pid == 0 {
+			unsafe {
+				exit(0);
+			}
+		}
+		unsafe {
+			waitpid(pid, 0, 0);
+		}
+	})
+}
+
+fn vfork(lo: &mut Bencher) {
+	extern {
+		fn vfork() -> c_int;
+	}
+
+	lo.iter(|| {
+		let pid = unsafe {
+			vfork()
 		};
 		if pid == 0 {
 			unsafe {
