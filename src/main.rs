9eb0e5441ab59d9f937eef152d07dffde8228bff
Sol Boucher <sboucher@cmu.edu>
2020-03-25 01:53:10 -0400

    src/main.rs: Generate C header to standard output in response to $ cargo run

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+extern crate inger;
+
+use inger::ffi::Linger;
+use std::mem::size_of;
+
+fn main() {
+	print!(
+		"\
+		#ifndef LIBINGER_H_\n\
+		#define LIBINGER_H_\n\
+		\n\
+		#include <stdbool.h>\n\
+		#include <stdint.h>\n\
+		\n\
+		typedef struct {{\n\
+			bool is_complete;\n\
+			uint8_t continuation[{}];\n\
+		}} linger_t;\n\
+		\n\
+		linger_t launch(void (*)(void *), uint64_t, void *);\n\
+		void resume(linger_t *, uint64_t);\n\
+		\n\
+		#endif\n\
+		",
+		size_of::<Linger>(),
+	);
+}
