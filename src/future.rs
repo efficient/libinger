5c01dfcc0a816f86d99b485bbf45a4cb94b17bfc
Sol Boucher <sboucher@cmu.edu>
2021-02-25 17:08:36 -0500

    src/future.rs: Add more flexible poll_fns() that supporting two poll preexecution hooks
    The first hook is run from directly within poll(), and returns a second hook that is run from within
    the preemptible function after resume()'s normal scaffolding has run.
    
    This can be used, for instance, to copy TLS variables into the preemptible function's private TCB.

--- src/future.rs
+++ src/future.rs
@@ -2,24 +2,45 @@ use linger::Linger;
 use std::future::Future;
 use std::io::Result;
 use std::pin::Pin;
+use std::sync::mpsc::SyncSender;
 use std::task::Context;
 use std::task::Poll;
 use std::thread::Result as ThdResult;
 
-pub struct PreemptiveFuture<T, F: FnMut(*mut Option<ThdResult<T>>) + Send> {
+pub struct PreemptiveFuture<
+	T,
+	F: FnMut(*mut Option<ThdResult<T>>) + Send,
+	P: Fn() -> I + Unpin,
+	I: FnMut() + Unpin,
+> {
 	fun: Option<Linger<T, F>>,
 	us: u64,
+	poll: P,
+	pre: SyncSender<I>,
 }
 
-pub fn poll_fn<T: Send>(mut fun: impl FnMut() -> Poll<T> + Send, us: u64)
--> Result<PreemptiveFuture<T, impl FnMut(*mut Option<ThdResult<T>>) + Send>> {
+pub fn poll_fn<T: Send>(fun: impl FnMut() -> Poll<T> + Send, us: u64)
+-> Result<PreemptiveFuture<T, impl FnMut(*mut Option<ThdResult<T>>) + Send, impl Fn() -> fn(), fn()>> {
+	fn nop() {}
+	fn nope() -> fn() { nop }
+	poll_fns(nope, fun, us)
+}
+
+pub fn poll_fns<T: Send, I: FnMut() + Send + Unpin>(
+	poll: impl Fn() -> I + Unpin,
+	mut fun: impl FnMut() -> Poll<T> + Send,
+	us: u64,
+) -> Result<PreemptiveFuture<T, impl FnMut(*mut Option<ThdResult<T>>) + Send, impl Fn() -> I, I>> {
 	use linger::launch;
 	use linger::pause;
 	use std::hint::unreachable_unchecked;
+	use std::sync::mpsc::sync_channel;
 
+	let (pre, prep): (SyncSender<I>, _) = sync_channel(1);
 	let fun = Some(launch(move || {
 		let mut res;
 		while {
+			prep.recv().unwrap()();
 			res = fun();
 			res.is_pending()
 		} {
@@ -36,16 +57,20 @@ pub fn poll_fn<T: Send>(mut fun: impl FnMut() -> Poll<T> + Send, us: u64)
 	Ok(PreemptiveFuture {
 		fun,
 		us,
+		poll,
+		pre,
 	})
 }
 
-impl<T, F: FnMut(*mut Option<ThdResult<T>>) + Send> Future for PreemptiveFuture<T, F> {
+impl<T, F: FnMut(*mut Option<ThdResult<T>>) + Send, P: Fn() -> I + Unpin, I: FnMut() + Unpin>
+Future for PreemptiveFuture<T, F, P, I> {
 	type Output = Result<T>;
 
 	fn poll(mut self: Pin<&mut Self>, context: &mut Context) -> Poll<Self::Output> {
 		use linger::resume;
 
 		if let Some(mut fun) = self.fun.take() {
+			self.pre.send((self.poll)()).unwrap();
 			if let Err(or) = resume(&mut fun, self.us) {
 				Poll::Ready(Err(or))
 			} else {
