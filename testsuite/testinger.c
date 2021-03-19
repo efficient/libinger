993935db1042d9741da7597d9666fb547fd3a80d
Sol Boucher <sboucher@cmu.edu>
2021-03-19 17:14:15 -0400

    testsuite: Add wrapper library for running vanilla programs in a preemptible function
    This will allow us to run third-party test suites, and also serves as a quick-and-dirty example of
    how to combine additional static libgotcha client librarie(s) into the libinger distribution.
    
    Although libgotcha doesn't provide a way to run code just before a program's main(), we can
    accomplish this by intercepting the __libc_start_main() call from crt's _start() implementation.

---
+++ testsuite/testinger.c
@@ -0,0 +1,57 @@
+#include "libinger.h"
+
+#include <stdio.h>
+#include <string.h>
+
+struct inout {
+	int argc;
+	char **argv;
+	char **envp;
+	int retval;
+};
+
+static int (*mainfunc)(int, char **, char **);
+
+static void testinging(void *inout) {
+	struct inout *argret = inout;
+	argret->retval = mainfunc(argret->argc, argret->argv, argret->envp);
+}
+
+static int testinger(int argc, char **argv, char **envp) {
+	struct inout argret = {
+		.argc = argc,
+		.argv = argv,
+		.envp = envp,
+	};
+	launch(testinging, UINT64_MAX, &argret);
+	return argret.retval;
+}
+
+#pragma weak libtestinger_getenv = getenv
+const char *getenv(const char *name) {
+	(void) name;
+	return "libtestinger.so";
+}
+
+#pragma weak libtestinger_libc_start_main = __libc_start_main
+int __libc_start_main(int (*main)(int, char **, char **), int argc, char**argv, int (*init)(int, char **, char **), void (*fini)(void), void (*rtld_fini)(void), void *stack_end) {
+	const char *skiplist = getenv("LIBGOTCHA_SKIP");
+	if(skiplist && strstr(skiplist, *argv))
+		return __libc_start_main(main, argc, argv, init, fini, rtld_fini, stack_end);
+
+	if(getenv("LIBTESTINGER_VERBOSE")) {
+		fputs("!!! LD_PRELOAD=.../libtestinger.so", stderr);
+		for(char **arg = argv; arg != argv + argc; ++arg)
+			fprintf(stderr, " %s", *arg);
+		fputc('\n', stderr);
+	}
+
+	mainfunc = main;
+	return __libc_start_main(testinger, argc, argv, init, fini, rtld_fini, stack_end);
+}
+
+#pragma weak libtestinger_signal = signal
+void (*signal(int signum, void (*handler)(int)))(int) {
+	(void) signum;
+	return handler;
+}
