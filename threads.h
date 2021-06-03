2d6846bfcfb8ccd564ed4feae2fed10bb1d112db
Sol Boucher <sboucher@cmu.edu>
2021-06-03 01:06:53 -0400

    threads.h: Remove unintentional dependency on multilib toolchains
    It turns out that /usr/include/sys isn't always present unless glibc's i386 development headers are
    installed, so we switch to bouncing off a different directory that we can rely on.

--- threads.h
+++ threads.h
@@ -3,7 +3,7 @@
 
 #ifndef __STDC_NO_THREADS__
 // Defer to the SYStem header.  This works as long as there's no sys/ folder in our tree.
-# include <sys/../threads.h>
+# include <x86_64-linux-gnu/../threads.h>
 #else
 # define thread_local _Thread_local
 #endif
