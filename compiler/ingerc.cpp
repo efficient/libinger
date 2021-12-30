08355b6cb34c287019c2853a22d83df723e8c59d
Sol Boucher <sboucher@cmu.edu>
2021-12-29 22:32:36 -0500

    compiler: Move the end label corresponding to each landing pad down to the destructor call
    This prevents leaks between the end of the basic block and when the destructor is called on the
    normal codepath.

--- compiler/ingerc.cpp
+++ compiler/ingerc.cpp
@@ -1,12 +1,16 @@
 #include "llvm/ADT/BitVector.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/IR/GlobalValue.h"
 #include "llvm/IR/LegacyPassManager.h"
 
 #include <dlfcn.h>
 
 using llvm::legacy::PassManager;
+using llvm::GlobalValue;
 using llvm::MachineFunction;
 using llvm::MachineFunctionPass;
+using llvm::MachineInstr;
+using llvm::MachineInstrBundleIterator;
 using llvm::Pass;
 using llvm::PassInfo;
 using llvm::PassRegistry;
@@ -16,12 +20,99 @@ using llvm::outs;
 namespace {
 struct IngerCancel: public MachineFunctionPass {
 	virtual bool runOnMachineFunction(MachineFunction &mf) override {
-		outs() << "runOnMachineFunction(" << mf.getName() << ")\n";
-		return false;
+		if(mf.getName().contains("drop_in_place"))
+			return false;
+		outs() << "FUNCTION: " << mf.getName() << '\n';
+
+		auto changed = false;
+		for(auto &pad : mf.getLandingPads()) {
+			auto &beginLabel = *pad.BeginLabels.front();
+			auto &endLabel = *pad.EndLabels.front();
+			outs() << "landing pad: " << beginLabel << ' ' << endLabel << '\n';
+
+			auto &cleanupBlock = *pad.LandingPadBlock;
+			outs() << cleanupBlock << '\n';
+
+			auto dropCall = std::find_if(
+				cleanupBlock.begin(),
+				cleanupBlock.end(),
+				[](auto &each) {
+					return isCallTo(each, [](auto &fun) {
+						return fun.getName().contains("drop_in_place");
+					});
+				}
+			);
+			if(dropCall != cleanupBlock.end()) {
+				outs() << "dropCall: " << *dropCall << '\n';
+
+				auto endInst = findInst(mf, [&endLabel](auto &each) {
+					return std::any_of(
+						each.operands_begin(),
+						each.operands_end(),
+						[&endLabel](auto &each) {
+							return each.isMCSymbol()
+								&& each.getMCSymbol() == &endLabel;
+						}
+					);
+				});
+				assert(endInst);
+				outs() << "endInst: " << **endInst << '\n';
+
+				auto nextInst = this->nextInst(*endInst);
+				while(nextInst && !isCallTo(**nextInst, [&dropCall](auto &fun) {
+					return &fun == dropCall->getOperand(0).getGlobal();
+				}))
+					nextInst = this->nextInst(*nextInst);
+				assert(nextInst);
+				outs() << "nextInst: " << **nextInst << '\n';
+
+				auto *movedInst = (*endInst)->removeFromParent();
+				(*nextInst)->getParent()->insert(*nextInst, movedInst);
+				changed = true;
+			}
+		}
+
+		return changed;
 	}
 
 	static char ID;
 	IngerCancel(): MachineFunctionPass(ID) {}
+
+private:
+	static std::optional<MachineInstrBundleIterator<MachineInstr>> findInst(
+		MachineFunction &mf,
+		std::function<bool(const MachineInstr &)> pred
+	) {
+		for(auto &block : mf) {
+			auto inst = std::find_if(block.begin(), block.end(), pred);
+			if(inst != block.end())
+				return std::optional(inst);
+		}
+		return {};
+	}
+
+	static bool isCallTo(
+		const MachineInstr &inst,
+		std::function<bool(const GlobalValue &)> name
+	) {
+		return inst.isCall() && name(*inst.getOperand(0).getGlobal());
+	}
+
+	static std::optional<MachineInstrBundleIterator<MachineInstr>> nextInst(
+		MachineInstrBundleIterator<MachineInstr> inst
+	) {
+		auto &block = *inst->getParent();
+		auto nextInst = inst;
+		++nextInst;
+		if(nextInst != block.end())
+			return std::optional(nextInst);
+
+		auto *nextBlock = inst->getParent()->getNextNode();
+		if(nextBlock)
+			return std::optional(nextBlock->begin());
+
+		return {};
+	}
 };
 }
 
