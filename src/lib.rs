22ec24aab61d1674d848666b53370d3c500f19cb
Sol Boucher <sboucher@cmu.edu>
2022-02-17 20:56:13 -0500

    Enable libgotcha's new exit analysis optimization
    With this, the benchmarks show the same performance with and without global variable interception
    enabled, with the exception of the libset-reinitialization one.

--- src/lib.rs
+++ src/lib.rs
@@ -33,6 +33,9 @@ const QUANTUM_MICROSECS: u64  = 100;
 pub const STACK_N_PREALLOC: usize = Group::LIMIT;
 const STACK_SIZE_BYTES: usize = 2 * 1_024 * 1_024;
 
+#[no_mangle]
+static libgotcha_exitanalysis: bool = true;
+
 pub fn concurrency_limit() -> usize {
 	Group::limit()
 }
